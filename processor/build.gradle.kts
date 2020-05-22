e6a756eb57de65817ca19993cae756229de918b2
Thomas Broyer <t.broyer@ltgt.net>
2020-05-22 23:44:55 +0200

    Use auto-service-annotations in compile classpath

--- processor/build.gradle.kts
+++ processor/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.google.auto.service:auto-service:1.0-rc7")
+    compileOnly("com.google.auto.service:auto-service-annotations:1.0-rc7")
     annotationProcessor("com.google.auto.service:auto-service:1.0-rc7")
 
     compileOnly("net.ltgt.gradle.incap:incap:0.3")
